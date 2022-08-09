51181c2276d1674bbc0fc361d24c223e8a1f9cba
Alexandr Remizov <ALVRem@mail.ru>
2022-08-09 10:43:38 +0300

    Add sql fucthion and conect to docker container

---
+++ sql/db_postgres.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql+psycopg2://postgres:1@172.17.0.2/postgres",
+                       echo=True,
+                       )
+engine.connect()
+print(engine)
